c51b409432022316420c53cac8f0801cea845cdf
KnockKnock3 <knockknock3offical@gmail.com>
2025-09-02 15:19:20 +0100

    Added questions and css positioning

---
+++ script.js
@@ -0,0 +1,36 @@
+const questions = [
+    { question: "What is 3 × 3?", answer: 9 },
+    { question: "What is the square root of 121?", answer: 11 },
+    { question: "How many players are on a football (soccer) team?", answer: 11 },
+    { question: "What is 81 ÷ 9?", answer: 9 },
+    { question: "What is 5 + 4?", answer: 9 },
+    { question: "What is 15 - 4?", answer: 11 },
+    { question: "How many sides does a nonagon have?", answer: 9 },
+    { question: "What is 99 ÷ 9?", answer: 11 }
+];
+
+question_count = 0
+questions_correct = 0
+questions_total = questions.length
+
+document.getElementById("question_text").innerHTML = questions[question_count].question
+
+function process_answer(answer) {
+    if (questions[question_count].answer == answer) {
+        questions_correct += 1
+    }
+
+    question_count += 1
+
+    document.getElementById("question_text").innerHTML = questions[question_count].question
+    document.getElementById("question_number").innerHTML = "Question " + (question_count + 1)
+    document.getElementById("percentage").innerHTML = Math.round(100 * questions_correct/question_count) + "%"
+}
+
+function nine_button_clicked() {
+    process_answer(9)
+}
+
+function eleven_button_clicked() {
+    process_answer(11)
+}
